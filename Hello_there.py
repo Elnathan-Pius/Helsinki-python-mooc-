9b8292f7b57fa3404f4e63cb651ea63a4c5479b8
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 20:55:07 +0100

    added notes on what I learnt on the Hello_there.py exercise

--- Hello_there.py
+++ Hello_there.py
@@ -1,1 +1,4 @@
+# programming exercise: Hello there
+# description: write a program printing the quotation "Hello there!"
+# Note - you can use single quotations instead of double quotations for your print commands for something like printing an actual quotation
 print('print("Hello there!")')
