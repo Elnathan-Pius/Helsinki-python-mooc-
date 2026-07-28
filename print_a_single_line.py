9164bde02498dbbb29fbf36ac06108a17dc0a52f
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 19:07:38 +0100

    Update print_a_single_line.py

--- print_a_single_line.py
+++ print_a_single_line.py
@@ -1,3 +1,6 @@
+# programming exercise: Print a single line
+# description: structuring the format of how a line is printed
+# AI Note : Used AI to understand the rules of line layout format which the exercise wanted
 print(5,end="")
 print(" + ",end="")
 print(8,end="")
