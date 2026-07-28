36808d0fda0ee1f03e628f39f6991f25fe993e60
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 19:01:45 +0100

    Update Times_five.py

--- Times_five.py
+++ Times_five.py
@@ -1,5 +1,5 @@
 # programming exercise: Times Five
 # description: Input handling and integer multiplication
-# AI Note : Used AI to assist in understanding how comma seperation creates spaces
+# AI Note : Used AI to assist in understanding how comma acts as seperation and creates spaces
 x = int(input("Please type in a number"))
 print(x,"times 5 is", x*5)
