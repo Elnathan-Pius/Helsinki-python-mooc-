d772e0ffa983caac059c76ebf39d44f418967295
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 21:06:39 +0100

    adding a description to my name_and_exclamation.py exercise

--- name_and_exclamation.py
+++ name_and_exclamation.py
@@ -1,2 +1,4 @@
+# programming exercise: Name and exclamation
+# description: write a program which prints your name with exclamation marks
 name = input("What is your name?")
 print("!" + name + "!" + name + "!")
