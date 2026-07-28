9926abc8a5cad8b79e2eaf891fd6abf2f5be536a
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 18:45:57 +0100

    Update Arithmetics.py

--- Arithmetics.py
+++ Arithmetics.py
@@ -1,3 +1,6 @@
+# programming exercise: Arithmetic
+# description: practiced calculations with variables
+# AI Note : Used AI to help with the final layout and manage the spacing bettween the variables
 x = 27
 y = 15
 print(x,"+", y, "=" , (x)+ (y))
