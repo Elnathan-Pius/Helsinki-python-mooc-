e208f29ff7514b1c175696dd4962a951f0620c97
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-28 18:54:42 +0100

    Update Name_and_address.py

--- Name_and_address.py
+++ Name_and_address.py
@@ -1,3 +1,6 @@
+# programming exercise: Name and Address
+# description: profile layout using multiple lines
+# AI Note : Used AI to help with formatting and understand that you can put white space in quotation marks to add a space
 Given_name = input("What is your given name?")
 Family_name = input("What is your family name?")
 Street_address = input("What is your street address?")
