1b5e0293c28a1e808842e53bf55e24915e63ab4c
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-30 10:29:02 +0100

    Update and rename Fix_the_code_Product.py to 17_Fix_the_code_Product.py

--- 17_Fix_the_code_Product.py
+++ 17_Fix_the_code_Product.py
@@ -1,3 +1,5 @@
+# programming exercise: Fix the code - Product
+# description: Fix the code so it displays the products of the three number the user inputs
 number1 = int(input("Please type in the first number: "))
 number2 = int(input("Please type in the second number: "))
 number3 = int(input("Please type in the third number: "))
