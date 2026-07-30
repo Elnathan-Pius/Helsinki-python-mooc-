41e471abf0a1557cbba15a247f5c082c35d5087b
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-30 10:19:09 +0100

    Update and rename Utterances.py to 09_Utterances.py

---
+++ 09_Utterances.py
@@ -0,0 +1,6 @@
+# programming exercise: utterances
+# description: Fix the code 
+The_1st_part = input("The 1st part: ")
+The_2nd_part = input("The 2nd part: ")
+The_3rd_part = input("The 3rd part: ")
+print(The_1st_part+"-"+The_2nd_part+"-"+The_3rd_part+"!")
