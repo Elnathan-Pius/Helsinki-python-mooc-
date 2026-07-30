fbfa83bccc0a832efbeea9d0f95ee7bd80ca08d2
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-30 10:24:14 +0100

    Update and rename Name_and_age.py to 15_Name_and_age.py

---
+++ 15_Name_and_age.py
@@ -0,0 +1,6 @@
+# programming exercise: Name and Age
+# description: Using code to display someones age
+(name) = input("What is your name?")
+year = input("What year were you born?")
+(year1) = 2021 - int(year)
+print("Hi",name+", you will be",year1,"years old at the end of the year 2021")
--- Name_and_age.py
+++
@@ -1,4 +0,0 @@
-(name) = input("What is your name?")
-year = input("What year were you born?")
-(year1) = 2021 - int(year)
-print("Hi",name+", you will be",year1,"years old at the end of the year 2021")
