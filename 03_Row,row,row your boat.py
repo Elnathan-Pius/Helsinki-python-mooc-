8e7f71da61ac93b6ec7b29a32e0336ca4cb19a42
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-30 10:08:36 +0100

    Update and rename Row,row,row your boat.py to 03_Row,row,row your boat.py

---
+++ 03_Row,row,row your boat.py
@@ -0,0 +1,6 @@
+# programming exercise: print the ryhme
+# description: print the "Row,row,row your boat" song
+print("Row,row,row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
