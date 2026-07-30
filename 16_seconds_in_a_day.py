3e302b24c0a63826dad8845c9271133bdb83bf30
Elnathan-Pius <Elnathanpius8@gmail.com>
2026-07-30 10:27:11 +0100

    Update and rename seconds_in_a_day.py to 16_seconds_in_a_day.py

---
+++ 16_seconds_in_a_day.py
@@ -0,0 +1,5 @@
+# programming exercise: seconds in a day
+# description: using code display the amount of seconds in a day or multiple depending what the user wants
+days = input("How many days?")
+sec = (int(days) * 86400)
+print("Seconds in that many days:", sec)
--- seconds_in_a_day.py
+++
@@ -1,3 +0,0 @@
-days = input("How many days?")
-sec = (int(days) * 86400)
-print("Seconds in that many days:", sec)
